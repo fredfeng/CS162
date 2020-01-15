9e18410f5cb9ef000ace6b9b506c93a562287584
Yu Feng <fengyu8299@gmail.com>
2020-01-15 15:38:18 -0800

    typo

--- homework/listOps.ml
+++ homework/listOps.ml
@@ -6,6 +6,13 @@ let duplicate list =
 let rev list =
 (* to be written *)
 
+(* Converse a string to a list of characters. *)
+let explode s =
+  let rec exp i l =
+    if i < 0 then l else exp (i - 1) (s.[i] :: l) in
+  exp (String.length s - 1) []
+;;
+
 (* Find out whether a list is a palindrome *)
-let is_palindrome list =
+let is_palindrome str =
 (* to be written *)
