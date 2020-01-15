1cfee432772ec93466b5b3f99c942d9ffa71090d
Yu Feng <fengyu8299@gmail.com>
2020-01-14 23:14:44 -0800

    hw1

---
+++ homework/listOps.ml
@@ -0,0 +1,11 @@
+(* Duplicate the elements of a list *)
+let duplicate list = 
+(* to be written *)
+
+(* Reverse a list *)
+let rev list =
+(* to be written *)
+
+(* Find out whether a list is a palindrome *)
+let is_palindrome list =
+(* to be written *)
