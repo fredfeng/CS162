d31cf096e549bd5b6f1fce577db8ec18c4ce7625
Junrui Liu <junrui@ucsb.edu>
2025-01-13 11:53:39 -0800

    .

--- homework/hw1/lib/part2.ml
+++ homework/hw1/lib/part2.ml
@@ -4,8 +4,10 @@ open Util
 let rec compress (equal : 'a -> 'a -> bool) (xs : 'a list) : 'a list = todo ()
 let max (xs : int list) : int option = todo ()
 let rec join (xs : 'a option list) : 'a list option = todo ()
-let insert (k : 'k) (v : 'v) (d : ('k * 'v) list) : ('k * 'v) list = (k, v) :: d
 
-let rec lookup (equal : 'k -> 'k -> bool) (k : 'k) (d : ('k * 'v) list) :
+let insert (key : 'k) (value : 'v) (dict : ('k * 'v) list) : ('k * 'v) list =
+  (key, value) :: dict
+
+let rec lookup (equal : 'k -> 'k -> bool) (key : 'k) (dict : ('k * 'v) list) :
     'v option =
   todo ()
